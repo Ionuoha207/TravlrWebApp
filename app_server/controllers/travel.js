e8d83fee28ca081afaaff06a541cfb41cdc9263b
Ikechukwu <ikieonuoha@gmail.com>
2024-07-17 23:58:20 +0300

    Module 3 completed baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,4 +1,11 @@
 // app_server/routes/travel.js
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
+module.exports.travelList = function(req, res) {
+    res.render('travel', { title: 'Travlr Getaways', trips: trips });
+};
+
 var express = require('express');
 var router = express.Router();
 var controller = require('../controllers/travlr');
